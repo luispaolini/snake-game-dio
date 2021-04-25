6bb5ba0bdaa85b69e791e5c3e4a48a77365d8877
Luis Paolini <luis.paolini@gmail.com>
2021-04-25 17:32:09 -0300

    criar comida

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ snake[0] = {
 
 let direction = "right";
 
+let food = {
+  x: Math.floor(Math.random() * 15 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 1) * box,
+};
+
 function criarBG() {
   context.fillStyle = "lightgreen";
 
@@ -22,6 +27,11 @@ function criarCobrinha() {
 
     context.fillRect(snake[i].x, snake[i].y, box, box);
   }
+}
+
+function drawFood() {
+  context.fillStyle = "red";
+  context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener("keydown", update);
@@ -64,6 +74,8 @@ function iniciarJogo() {
   criarBG();
 
   criarCobrinha();
+
+  drawFood();
 
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
